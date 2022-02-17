1c479e8e4d7d4c187e7d9dcfa225e9f6f4784bbe
woftis <stevenwalker1991@googlemail.com>
2022-02-17 15:30:03 +0000

    added options or rainbow mode or eraser mode

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function orchestrateNewGrid(){
    //draw the new grid
    drawGrid();
 
-   //store the cells and listen to them for events
+   //store the created cells and listen to them for events
    listenToCells();
 
 }
@@ -19,7 +19,7 @@ function drawGrid() {
     for (let i = 0; i < squareGrid * squareGrid; i++) {
         //create a div for each cell and append it to the canvas
         let div = document.createElement("div");
-        div.classList.add(`cell`, `cell-${i}`);
+        div.classList.add(`cell`, `cell-${i}`, `cell-border`);
         canvas.append(div);
     }
 
@@ -30,7 +30,7 @@ function drawGrid() {
 
 //iterate through each cell and remove it from the DOM
 function removeGrid() {
-    document.querySelectorAll('.cell').forEach(e => e.remove());
+    document.querySelectorAll('.cell').forEach(cell => cell.remove());
 }
 
 function listenToCells(){
@@ -42,23 +42,74 @@ function listenToCells(){
 }
 
 function colourCell(){
-    this.style.backgroundColor = 'black';
+    if (mode == 'white-mode') {
+        this.style.backgroundColor = 'white';
+    } else if (mode == 'eraser-mode') {
+        this.style.backgroundColor = '';
+    } else if (mode == 'rainbow-mode') {
+        this.style.backgroundColor = generateRandomRGB();
+    }
 }
 
-/*
-    set up event listener on hover
-    write a function that generates random RGB
-*/
+function generateRandomRGB(){
+    let rgb = 'rgb('
+
+    for (i = 0; i < 3; i++) {
+        if (i < 2) {
+            rgb +=  `${Math.floor(Math.random() * 99)}, `;
+        } else {
+            rgb +=  `${Math.floor(Math.random() * 99)})`; 
+        }
+    }
+    return(rgb);
+}
+
+function toggleBorders(){
+    console.log('toggle');
+    document.querySelectorAll('.cell').forEach(cell => cell.classList.toggle("cell-border"));
+    toggleBorder.classList.toggle("selected");
+}
+
+function selectMode(){
+    //end the function if the mode selected is already activated 
+    if (this.id == mode) {
+        return;
+    }
+
+    //get the button of the mode crrently existed and change the class so no longer active
+    const currentSelectedBtn = document.getElementById(mode);
+    currentSelectedBtn.classList.toggle("selected");
+
+    //update the mode variable and highlight selected button
+    mode = this.id;
+    this.classList.toggle("selected");
+    
+;}
 
 const userCanvasSize = document.getElementById('number');
+const toggleBorder = document.getElementById('borders');
+const clearBtn = document.getElementById('clear');
+const modeBtns = document.querySelectorAll('.mode');
 
 //draw the first grid based on default value
 orchestrateNewGrid();
 
-//store the cells so we can add an event listener
-let canvasCells = document.querySelectorAll('.cell');
+//default the mode to standard
+let mode = "white-mode"
 
 
 //listen for changes on thee size of the canvas
-userCanvasSize.addEventListener('change', orchestrateNewGrid); 
+userCanvasSize.addEventListener('change', 
+orchestrateNewGrid); 
 
+//listener to toggle the grid on or off
+toggleBorder.addEventListener('click', toggleBorders);
+
+//listener to clear the grid
+clearBtn.addEventListener('click', orchestrateNewGrid);
+
+
+//listener for mode changes
+modeBtns.forEach(btn => btn.addEventListener('click', selectMode));
+
+generateRandomRGB();
