01c716f31631ddb23d0b23c59d8254b5e2d8c9d6
woftis <stevenwalker1991@googlemail.com>
2022-02-17 12:42:56 +0000

    first working iteration of mvp - includes colour in when cells are hovered over

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+function orchestrateNewGrid(){ 
+    //clear down existing grid
+   removeGrid();
 
+   //draw the new grid
+   drawGrid();
+
+   //store the cells and listen to them for events
+   listenToCells();
+
+}
 
 function drawGrid() {
-    //clear down existing grid
-    removeGrid();
-
+ 
     //get the latest value from user input
     let squareGrid = userCanvasSize.value;
     const canvas = document.querySelector('.canvas');
@@ -20,17 +28,37 @@ function drawGrid() {
 
 }
 
-//iterate through each cekk abd remove it from the DOM
+//iterate through each cell and remove it from the DOM
 function removeGrid() {
     document.querySelectorAll('.cell').forEach(e => e.remove());
 }
 
+function listenToCells(){
+    //find all the cells
+    let canvasCells = document.querySelectorAll('.cell');
+
+    //listen to hover on the cells so we can change colour
+    canvasCells.forEach(cell => cell.addEventListener ('mouseenter', colourCell));
+}
+
+function colourCell(){
+    this.style.backgroundColor = 'black';
+}
+
 /*
     set up event listener on hover
-    explore if class can provide random colour or if this needs to be done on background colour
     write a function that generates random RGB
 */
 
 const userCanvasSize = document.getElementById('number');
-drawGrid();
-userCanvasSize.addEventListener('change', drawGrid);
+
+//draw the first grid based on default value
+orchestrateNewGrid();
+
+//store the cells so we can add an event listener
+let canvasCells = document.querySelectorAll('.cell');
+
+
+//listen for changes on thee size of the canvas
+userCanvasSize.addEventListener('change', orchestrateNewGrid); 
+
